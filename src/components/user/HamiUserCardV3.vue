e7c76cf282fc2f1f997cadd8a64b05cff6fbd10b
wang3 <2780348784@qq.com>
2023-08-25 22:21:16 +0800

    add some code

--- src/components/user/HamiUserCardV3.vue
+++ src/components/user/HamiUserCardV3.vue
@@ -16,16 +16,11 @@ const userStore = useUserStore
 //watch
 
 //fun
-const handleFollow = () => {
-    $message.notifyError("暂不支持")
-}
-const handleChat = () => {
-    $message.notifyError("暂不支持")
-}
+
 </script>
 <template>
-    <div class="hami-user-card-v2">
-        <div class="user-card-v2-container">
+    <div class="hami-user-card-v3">
+        <div class="user-card-v3-container">
             <el-avatar :src="defaultAvatar" size="large" class="user-card-avatar"></el-avatar>
             <div class="user-card-body">
                 <div class="info-box">
@@ -33,18 +28,18 @@ const handleChat = () => {
                     <div class="profile-item">
                         <div class="follow followers">
                             <div class="count">1</div>
-                            <div class="text">粉丝</div>
+                            <div class="content">粉丝</div>
                         </div>
                         <el-divider direction="vertical"></el-divider>
                         <div class="follow followings">
                             <div class="count">1</div>
-                            <div class="text">关注</div>
+                            <div class="content">关注</div>
                         </div>
                         <el-divider direction="vertical"></el-divider>
-                        <div class="follow">
-                            在Hami创作的第
+                        <div class="follow time">
+                            <div class="content">在Hami创作的第</div>
                             <div class="count">600</div>
-                            天
+                            <div class="content">天</div>
                         </div>
                     </div>
                 </div>
@@ -54,19 +49,19 @@ const handleChat = () => {
 </template>
 
 <style scoped lang="less">
-.hami-user-card-v2 {
-    padding: 30px 24px;
+.hami-user-card-v3 {
+    padding: 20px 24px;
     border-radius: var(--hami-radius-medium);
     background-color: var(--hami-bg);
     margin-bottom: 20px;
 
-    .user-card-v2-container {
+    .user-card-v3-container {
         display: flex;
     }
 
     .user-card-avatar {
-        --el-avatar-size-large: 100px;
-        --el-avatar-size: 100px;
+        --el-avatar-size-large: 80px;
+        --el-avatar-size: 80px;
 
         :deep(img) {
             transition: all 1.5s linear;
@@ -101,14 +96,17 @@ const handleChat = () => {
                 height: 28px;
             }
 
-            .icon {
-                margin-right: 8px;
-            }
             .follow {
                 display: flex;
                 align-items: center;
                 .count {
                     color: var(--hami-text-3);
+                    margin-right: 6px;;
+                }
+            }
+            .follow.time {
+                .count {
+                    margin-left: 6px;
                 }
             }
         }
@@ -119,46 +117,7 @@ const handleChat = () => {
             font-weight: 700;
             height: 24px;
             line-height: 24px;
-            margin: 4px 0;
-        }
-
-        .work {
-            display: flex;
-            align-items: center;
-            color: var(--hami-text-gray);
-
-            .divider {
-                margin: 0 10px;
-            }
-        }
-
-        .profile {
-            max-width: 240px;
-        }
-
-        .blog {
-            max-width: 240px;
-            .content {
-                line-height: 29px;
-            }
-        }
-
-        .button-box {
-            min-width: 200px;
-            max-width: 252px;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-        }
-
-        .bottom {
-            display: flex;
-            align-items: center;
-            padding-left: 40px;
-
-            button {
-                width: 100px;
-            }
+            margin: 4px 0 16px 0;
         }
     }
 }
--- src/views/Creator.vue
+++ src/views/Creator.vue
@@ -1,23 +1,101 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted, computed } from "vue"
+import { onMounted, ref } from "vue"
 import { useRoute, useRouter } from "vue-router"
 //interface
 
 //router, props, inject, provide
-
+const $router = useRouter()
+const $route = useRoute()
 //custom var
+const menuStyle = {
+    "--el-menu-text-color": "#515767",
+    "--el-menu-active-color": "#1e80ff",
+    "--active-bg-color": "#eaf2ff",
+    "--el-menu-item-font-size": "16px",
+    "--el-menu-hover-bg-color": ""
+}
 
+const frameMode = ref("/creator/home")
+
+onMounted(() => {
+    frameMode.value = $route.path
+    console.log("Creator mounted");
+})
 //life cycle
 
 //watch
 
 //fun
+const handleSelect = (index: string, indexPath: string) => {
+    console.log("handle Select");
+    console.log(index, indexPath);
+    frameMode.value = index
+    $router.push(index)
+}
+
+const handleOpen = () => {
+
+}
+const handleClose = () => {
+
+}
+
+const handleClick = () => {
+
+}
 
 </script>
 <template>
     <div class="hami-creator">
         <div class="hami-creator-container">
-            <div class="creator-nav"></div>
+            <div class="creator-nav">
+                <el-button class="creator-nav-title" type="primary" size="large" @click="handleClick">写文章</el-button>
+                <el-menu
+                    :default-active="frameMode"
+                    @select="handleSelect"
+                    @open="handleOpen"
+                    @close="handleClose" :style="menuStyle">
+                    <el-menu-item index="/creator/home">
+                        <el-icon>
+                            <HomeFilled/>
+                        </el-icon>
+                        <span>首页</span>
+                    </el-menu-item>
+                    <el-sub-menu index="/creator/content">
+                        <template #title>
+                            <el-icon>
+                                <Grid/>
+                            </el-icon>
+                            <span>内容管理</span>
+                        </template>
+                        <el-menu-item index="/creator/content/article">
+                            <span>文章管理</span>
+                        </el-menu-item>
+                    </el-sub-menu>
+                    <el-sub-menu index="/creator/tool">
+                        <template #title>
+                            <el-icon>
+                                <Tools/>
+                            </el-icon>
+                            <span>创作工具</span>
+                        </template>
+                        <el-menu-item index="/creator/tool/import">
+                            <span>文章导入</span>
+                        </el-menu-item>
+                    </el-sub-menu>
+                    <el-sub-menu index="/creator/help">
+                        <template #title>
+                            <el-icon>
+                                <QuestionFilled/>
+                            </el-icon>
+                            <span>帮助中心</span>
+                        </template>
+                        <el-menu-item index="/creator/help/question">
+                            <span>常见问题</span>
+                        </el-menu-item>
+                    </el-sub-menu>
+                </el-menu>
+            </div>
             <div class="creator-body">
                 <router-view></router-view>
             </div>
@@ -26,19 +104,52 @@ import { useRoute, useRouter } from "vue-router"
 </template>
 
 <style scoped lang="less">
-    .hami-creator {
-        margin-top: 40px;
-        .hami-creator-container {
-            max-width: 1200px;
-            margin: 0 auto;
-            display: flex;
-            //justify-content: space-between;
+.hami-creator {
+    margin-top: 40px;
+
+    .hami-creator-container {
+        max-width: 1200px;
+        margin: 0 auto;
+        display: flex;
+        //justify-content: space-between;
+    }
+
+    .creator-nav {
+        min-width: 240px;
+        background-color: var(--hami-bg);
+        padding: 16px 16px;
+        min-height: 500px;
+        border-radius: var(--hami-radius);
+        .creator-nav-title {
+            width: 100%;
+            margin-bottom: 20px;
         }
-        .creator-nav {
-            min-width: 240px;
+        .el-menu {
+            border: none;
         }
-        .creator-body {
-            flex: 1;
+        .el-menu-item {
+            border-radius: var(--hami-radius-small);
+            //margin-bottom: 6px;
+        }
+        :deep(.el-sub-menu) {
+            .el-menu-item:last-child {
+                margin-bottom: 0;
+            }
+        }
+        :deep(.el-menu-item.is-active ) {
+            background-color: var(--active-bg-color);
+        }
+        :deep(.el-sub-menu.is-active .el-sub-menu__title) {
+            color: var(--el-menu-active-color);
+        }
+        :deep(.el-sub-menu__icon-arrow) {
+            font-size: 16px;
         }
     }
+
+    .creator-body {
+        flex: 1;
+        margin-left: 32px;
+    }
+}
 </style>
